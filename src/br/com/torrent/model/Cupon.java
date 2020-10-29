0777f6abf70b3ecf8736e701c3a4bf816c7d997c
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-10-29 15:46:43 -0300

    Update Cupon.java
    variavel rando

--- src/br/com/torrent/model/Cupon.java
+++ src/br/com/torrent/model/Cupon.java
@@ -7,15 +7,18 @@ package br.com.torrent.model;
 // melhor gera essa o radon e a data no banco de dados postrgres
 // https://www.savepoint.blog.br/2015/11/26/gerando-valores-aleatorios-ou-quase/
 public class Cupon {
+
     private double porcentagem;
     private int data_geracao;
+    private int rando;
 
     public Cupon() {
     }
 
-    public Cupon(double porcentagem, int data_geracao) {
+    public Cupon(double porcentagem, int data_geracao, int rando) {
         this.porcentagem = porcentagem;
         this.data_geracao = data_geracao;
+        this.rando = rando;
     }
 
     public double getPorcentagem() {
@@ -33,7 +36,13 @@ public class Cupon {
     public void setData_geracao(int data_geracao) {
         this.data_geracao = data_geracao;
     }
-    
-    
-    
+
+    public int getRando() {
+        return rando;
+    }
+
+    public void setRando(int rando) {
+        this.rando = rando;
+    }
+
 }
