40c759dd64bf602ba73ea763aa098749c18c623d
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-11-03 22:47:53 -0300

    Update UsuarioBll.java
    Implementando a inteface

--- src/br/com/torrent/bll/UsuarioBll.java
+++ src/br/com/torrent/bll/UsuarioBll.java
@@ -1,50 +1,133 @@
 package br.com.torrent.bll;
 
 import br.com.torrent.dal.UsuarioDal;
+import br.com.torrent.interfaces.UsuarioInterface;
 import br.com.torrent.model.UsuarioModel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class UsuarioBll {
+public class UsuarioBll implements UsuarioInterface {
 
-    private UsuarioDal usuarioDao;
+    UsuarioInterface novoUsuario;
+    //private UsuarioDal usuarioDao;
 
-    public UsuarioBll(){
+    public UsuarioBll() throws Exception {
+        novoUsuario = new UsuarioDal();
+    }
+
+    @Override
+    public void adicionarUsuario(UsuarioModel usuario) {
         try {
-            usuarioDao = new UsuarioDal();
-        } catch (Exception ex) {
-            Logger.getLogger(UsuarioBll.class.getName()).log(Level.SEVERE, null, ex);
+            novoUsuario.adicionarUsuario(usuario);
+        } catch (Exception e) {
+            try {
+                throw new Exception("Erro bll incluir" + e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoBll.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
-    // 1°
-    public void adicionarUsuario(UsuarioModel usuario) throws Exception {
-        usuarioDao.adicionarUsuario(usuario);
+    @Override
+    public void deleteUsuario(UsuarioModel usuario) {
+        try {
+            novoUsuario.deleteUsuario(usuario);
+        } catch (Exception e) {
+            try {
+                throw new Exception(e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoBll.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
-    // 2°
-    public void removerUsuario(UsuarioModel usuario) throws Exception {
-        usuarioDao.deleteUsuario(usuario.getIden());
+    @Override
+    public void updateUsuario(UsuarioModel usuario) {
+        try {
+            novoUsuario.updateUsuario(usuario);
+        } catch (Exception e) {
+            try {
+                throw new Exception(e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoBll.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
     }
 
-    // 3°
-    public void alterarUsuario(UsuarioModel usuario) throws Exception {
-        usuarioDao.updateUsuario(usuario);
+    @Override
+    public ArrayList<UsuarioModel> getAllUsuario() {
+        try {
+            return novoUsuario.getAllUsuario();
+        } catch (Exception e) {
+            try {
+                throw new Exception(e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoBll.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
     }
 
-    // 4° 
-    public List<UsuarioModel> consultarUsuario() throws Exception {
-        return usuarioDao.getAllUsuario();
+    @Override
+    public UsuarioModel getUsuarioById(int id) {
+        try {
+            return novoUsuario.getUsuarioById(id);
+        } catch (Exception e) {
+            try {
+                throw new Exception(e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoBll.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
     }
 
-    // 5°
-    public UsuarioModel consultarUsuarioPorId(int id) throws Exception {
-        return usuarioDao.getUsuarioById(id);
+    @Override
+    public UsuarioModel findUsuarioName(String nome) {
+        try {
+            return novoUsuario.findUsuarioName(nome);
+        } catch (Exception e) {
+        }
+        return null;
     }
-        
-    public UsuarioModel consultarUsuarioPorNome(String nome) throws Exception {
-        return usuarioDao.getUsuarioByNome(nome);
-    }
-        
+
+//    public UsuarioBll() {
+//        try {
+//            usuarioDao = new UsuarioDal();
+//        } catch (Exception ex) {
+//            Logger.getLogger(UsuarioBll.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    // 1°
+//    public void adicionarUsuario(UsuarioModel usuario) throws Exception {
+//        usuarioDao.adicionarUsuario(usuario);
+//    }
+//
+//    // 2°
+//    public void removerUsuario(UsuarioModel usuario) throws Exception {
+//        usuarioDao.deleteUsuario(usuario.getIden());
+//    }
+//
+//    // 3°
+//    public void alterarUsuario(UsuarioModel usuario) throws Exception {
+//        usuarioDao.updateUsuario(usuario);
+//    }
+//
+//    // 4° 
+//    public List<UsuarioModel> consultarUsuario() throws Exception {
+//        return usuarioDao.getAllUsuario();
+//    }
+//
+//    // 5°
+//    public UsuarioModel consultarUsuarioPorId(int id) throws Exception {
+//        return usuarioDao.getUsuarioById(id);
+//    }
+//        
+//    public UsuarioModel consultarUsuarioPorNome(String nome) throws Exception {
+//        return usuarioDao.getUsuarioByNome(nome);
+//    }
 }
