79fb6922aaca23b66eb8e302bd9fed0015b0ccfb
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-11-03 22:47:48 -0300

    Update UsuarioDal.java
    Implementando a inteface

--- src/br/com/torrent/dal/UsuarioDal.java
+++ src/br/com/torrent/dal/UsuarioDal.java
@@ -1,5 +1,6 @@
 package br.com.torrent.dal;
 
+import br.com.torrent.interfaces.UsuarioInterface;
 import br.com.torrent.model.Cupon;
 import br.com.torrent.model.UsuarioModel;
 import br.com.torrent.util.Conexao;
@@ -10,50 +11,55 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class UsuarioDal {
+public class UsuarioDal implements UsuarioInterface {
 
-    private Connection conexao;
+    private Connection conect;
 
     public UsuarioDal() throws Exception {
-        conexao = Conexao.getInstance().getConnection();
+        this.conect = Conexao.getInstance().getConnection();
     }
 
-    public void adicionarUsuario(UsuarioModel usuario) throws Exception {
-
-        String sql = "INSERT INTO cup_usuarios (cup_nome, cup_cpf, cup_email, cup_senha) VALUES (?, ?, ?, ?)";
+    @Override
+    public void adicionarUsuario(UsuarioModel usuario) {
 
         try { // preparando a conexao;
-            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+
+            String sql = "INSERT INTO cup_usuarios (cup_nome, cup_cpf, cup_email, cup_senha) VALUES (?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conect.prepareStatement(sql);
             preparedStatement.setObject(1, usuario.getNome());
             preparedStatement.setObject(2, usuario.getCpf());
             preparedStatement.setObject(3, usuario.getEmail());
             preparedStatement.setObject(4, usuario.getSenha());
-
             preparedStatement.executeUpdate(); // executa o comando da String sql;
-        } catch (SQLException erro) {
-            throw new Exception("Error ao inserir registro" + erro.getMessage());
+        } catch (SQLException e) {
+            try {
+                throw new Exception("Erro ao incluir novo usuario!" + e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoDal.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
-    public void deleteUsuario(int id) throws Exception {
-        String sql = "DELETE FROM usuario WHERE usu_iden=?";
+    @Override
+    public void deleteUsuario(UsuarioModel usuario) {
+
         try {
-            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
+            String sql = "DELETE FROM usuario WHERE usu_iden=?";
+            PreparedStatement preparedStatement = conect.prepareStatement(sql);
+            preparedStatement.setInt(1, usuario.getIden());
             preparedStatement.executeUpdate();
-
         } catch (SQLException erro) {
-            throw new Exception("Ocorreu um erro ao deletar este registro!\n"
-                    + erro.getMessage());
         }
     }
 
-    public void updateUsuario(UsuarioModel usuario) throws Exception {
-        String sql = "UPDATE usuario set  usu_nome=?, usu_cpf=?, usu_email=?, usu_senha=?, where usu_cup_iden=?";
-
+    @Override
+    public void updateUsuario(UsuarioModel usuario) {
         try {
-            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            String sql = "UPDATE usuario set  usu_nome=?, usu_cpf=?, usu_email=?, usu_senha=?, where usu_cup_iden=?";
+            PreparedStatement preparedStatement = conect.prepareStatement(sql);
             preparedStatement.setString(1, usuario.getNome());
             preparedStatement.setInt(2, usuario.getIden());
             preparedStatement.setString(3, usuario.getEmail());
@@ -61,54 +67,68 @@ public class UsuarioDal {
             preparedStatement.executeUpdate();
 
         } catch (SQLException erro) {
-            throw new Exception("Ocorreu um erro ao alterar este registro\n"
-                    + erro.getMessage());
         }
     }
 
-    public List<UsuarioModel> getAllUsuario() throws Exception {
-        List<UsuarioModel> listUsuario = new ArrayList<UsuarioModel>();
+    @Override
+    public ArrayList<UsuarioModel> getAllUsuario() {
+        List<UsuarioModel> usuarios = new ArrayList<UsuarioModel>();
+
         try {
-            Statement statement = conexao.createStatement();
+            Statement statement = conect.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * FROM cup_usuarios");
             while (rs.next()) {
-                UsuarioModel usuario = new UsuarioModel();
-                usuario.setIden(rs.getInt("cup_iden"));
-                usuario.setNome(rs.getString("cup_nome"));
-                usuario.setEmail(rs.getString("cup_email"));
-                usuario.setSenha(rs.getString("cup_senha"));
-                usuario.setCpf(rs.getString("cup_cpf"));
-                listUsuario.add(usuario);
+                UsuarioModel novoUsuario = new UsuarioModel();
+                novoUsuario.setIden(rs.getInt("cup_iden"));
+                novoUsuario.setNome(rs.getString("cup_nome"));
+                novoUsuario.setEmail(rs.getString("cup_email"));
+                novoUsuario.setSenha(rs.getString("cup_senha"));
+                novoUsuario.setCpf(rs.getString("cup_cpf"));
+                usuarios.add(novoUsuario);
             }
-        } catch (SQLException erro) {
-            throw new Exception("Ocorreu um erro ao consultar os registros de fabricantes\n" + erro.getMessage());
+            //return usuarios;
+        } catch (Exception e) {
+            try {
+                throw new Exception("Erro ao Listar todos os usuarios: " + e.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(PlanoDal.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        return listUsuario;
+        return null;
+
     }
 
-    public UsuarioModel getUsuarioById(int id) throws Exception {
-        UsuarioModel usuario = new UsuarioModel();
-        String sql = "SELECT * FROM usuario WHERE usu_iden=?";
+    @Override
+    public UsuarioModel getUsuarioById(int id) {
+        UsuarioModel retUsuario = new UsuarioModel();
+
         try {
-            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            String sql = "SELECT * FROM usuario WHERE usu_iden=?";
+            PreparedStatement preparedStatement = conect.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             ResultSet rs = preparedStatement.executeQuery();
             if (rs.next()) {
-                usuario.setIden(rs.getInt("usu_id"));
-                usuario.setNome(rs.getString("usu_nome"));
-                usuario.setSenha(rs.getString("usu_senha"));
+                retUsuario.setIden(rs.getInt("usu_id"));
+                retUsuario.setNome(rs.getString("usu_nome"));
+                retUsuario.setSenha(rs.getString("usu_senha"));
             }
         } catch (Exception erro) {
-            throw new Exception("Erro ao buscar no banco de dados: Alunos!!\n" + erro.getMessage());
+            try {
+                throw new Exception("Erro ao buscar no banco de dados: Alunos!!\n" + erro.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(UsuarioDal.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        return usuario;
+        return retUsuario;
     }
 
-    public UsuarioModel getUsuarioByNome(String nome) throws Exception {
+    @Override
+    public UsuarioModel findUsuarioName(String nome) {
         UsuarioModel usuario = new UsuarioModel();
-        String sql = "SELECT * FROM cup_usuarios WHERE cup_nome=?";
+
         try {
-            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            String sql = "SELECT * FROM cup_usuarios WHERE cup_nome=?";
+            PreparedStatement preparedStatement = conect.prepareStatement(sql);
             preparedStatement.setString(1, nome);
             ResultSet rs = preparedStatement.executeQuery();
             if (rs.next()) {
@@ -117,7 +137,11 @@ public class UsuarioDal {
                 usuario.setSenha(rs.getString("cup_senha"));
             }
         } catch (Exception erro) {
-            throw new Exception("Erro ao buscar no banco de dados: Alunos!!\n" + erro.getMessage());
+            try {
+                throw new Exception("Erro ao buscar no banco de dados: Alunos!!\n" + erro.getMessage());
+            } catch (Exception ex) {
+                Logger.getLogger(UsuarioDal.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         return usuario;
     }
