0a726cca70d663da79abcec56249496c63601ba6
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-11-10 17:36:19 -0300

    Create CupomModel.java

---
+++ src/br/com/torrent/model/CupomModel.java
@@ -0,0 +1,44 @@
+package br.com.torrent.model;
+
+/**
+ *
+ * @author edivan
+ */
+// melhor gera essa o radon e a data no banco de dados postrgres
+// https://www.savepoint.blog.br/2015/11/26/gerando-valores-aleatorios-ou-quase/
+public class CupomModel {
+
+    private double porcentagem;
+    private String data_geracao;
+
+    public CupomModel() {
+    }
+
+    public CupomModel(double porcentagem, String data_geracao) {
+        this.porcentagem = porcentagem;
+        this.data_geracao = data_geracao;
+
+    }
+
+    public double getPorcentagem() {
+        return porcentagem;
+    }
+
+    public void setPorcentagem(double porcentagem) {
+        this.porcentagem = porcentagem;
+    }
+
+    public String getData_geracao() {
+        return data_geracao;
+    }
+
+    public void setData_geracao(String data_geracao) {
+        this.data_geracao = data_geracao;
+    }   
+
+    @Override
+    public String toString() {
+        return "Cupon{" + "porcentagem=" + porcentagem + ", data_geracao=" + data_geracao + '}';
+    }
+
+}
