c07306e58203d677385ed179cfc8054d708eb3ef
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-11-10 22:17:47 -0300

    Update CupomDal.java

--- src/br/com/torrent/dal/CupomDal.java
+++ src/br/com/torrent/dal/CupomDal.java
@@ -22,32 +22,18 @@ public class CupomDal implements CupomInterface {
         this.conect = Conexao.getInstance().getConnection();
     }
 
-//    public CupomModel gerarRadon(CupomModel idRandon) throws Exception {
-//        CupomModel cupon = new CupomModel();
-//        String sql = "SELECT round(random() * 59 + 1) id_radon";
-//
-//        try {
-//            PreparedStatement preparedStatement = conect.prepareStatement(sql);
-//            preparedStatement.setInt(1, idRandon);
-//            ResultSet rs = preparedStatement.executeQuery();
-//            if (rs.next()) {
-//                cupon.setPorcentagem(rs.getInt("id_radon"));
-//            }
-//        } catch (Exception erro) {
-//            throw new Exception("Erro ao buscar no banco de dados: Alunos!!\n" + erro.getMessage());
-//        }
-//        return cupon;
-//    }
     @Override
     public CupomModel gerarRadon(String cupon) {
         CupomModel retCupom = new CupomModel();
         try {
-            PreparedStatement prep = conect.prepareStatement("SELECT round(random() * 59 + 1) id_radon");
-            prep.setString(1, cupon);
+            PreparedStatement prep = conect.prepareStatement("SELECT cup_usuarios(round(random() * 59 + 1)) cup_porcentagem");
+            //prep.setString(1, cupon);
             ResultSet rs = prep.executeQuery();
-            if (rs.next()) {
-                retCupom.setPorcentagem(rs.getInt("id_radon"));
-            }
+            System.out.println(cupon);
+            retCupom.setPorcentagem(rs.getDouble("cup_porcentagem"));
+//            if (rs.next()) {
+//                retCupom.setPorcentagem(rs.getDouble("cup_porcentagem"));
+//            }
         } catch (Exception e) {
             try {
                 throw new Exception("Erro ao gerar o RANDOM!!\n" + e.getMessage());
@@ -55,7 +41,7 @@ public class CupomDal implements CupomInterface {
                 Logger.getLogger(CupomDal.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        return null;
+        return retCupom;
     }
 
 }
